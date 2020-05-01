faab4128514d220d16f8ddbe045cddbb6ffdfb4b
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-05-01 20:23:01 +0200

    Updates

---
+++ precache-manifest.9b4f01970e08573d395bd208cf768b55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef1ea87d28bae6e78e101af4bdf6bf0",
+    "url": "/React-Redux-shop/index.html"
+  },
+  {
+    "revision": "e230b9e51ecc98b81e7c",
+    "url": "/React-Redux-shop/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "53e4014ad46de9674ccb",
+    "url": "/React-Redux-shop/static/js/2.5398732b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-shop/static/js/2.5398732b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e230b9e51ecc98b81e7c",
+    "url": "/React-Redux-shop/static/js/main.6ed97175.chunk.js"
+  },
+  {
+    "revision": "0137cb15097554173759",
+    "url": "/React-Redux-shop/static/js/runtime-main.79ccf493.js"
+  }
+]);
